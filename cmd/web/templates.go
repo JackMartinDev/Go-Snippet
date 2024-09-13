505061ee0bb3b48cc894697c8841b83796ae9017
Jack Martin <jackmartin.dev@gmail.com>
2024-09-13 17:39:44 +0900

    Add template cache and render helper function

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,41 @@
 package main
 
-import "snippetbox.jackmartin.jp/internal/models"
+import (
+	"html/template" // New import
+	"path/filepath" // New import
+	"snippetbox.jackmartin.jp/internal/models"
+)
 
 type templateData struct {
 	Snippet  models.Snippet
 	Snippets []models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+
+		files := []string{
+			"./ui/html/base.tmpl",
+			"./ui/html/partials/nav.tmpl",
+			page,
+		}
+
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	// Return the map.
+	return cache, nil
 }
