ae46cd4d6d655388ebe993afa574a0b15884d34d
Jack Martin <jackmartin.dev@gmail.com>
2024-09-18 15:21:16 +0900

    Add auth middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -10,18 +10,22 @@ func (app *application) routes() http.Handler {
 
 	mux.Handle("GET /{$}", sessionWrapper(app.home, app))
 	mux.Handle("GET /snippet/view/{id}", sessionWrapper(app.snippetView, app))
-	mux.Handle("GET /snippet/create", sessionWrapper(app.snippetCreate, app))
-	mux.Handle("POST /snippet/create", sessionWrapper(app.snippetCreatePost, app))
+	mux.Handle("GET /snippet/create", authSessionWrapper(app.snippetCreate, app))
+	mux.Handle("POST /snippet/create", authSessionWrapper(app.snippetCreatePost, app))
 
 	mux.Handle("GET /user/signup", sessionWrapper(app.userSignup, app))
 	mux.Handle("POST /user/signup", sessionWrapper(app.userSignupPost, app))
 	mux.Handle("GET /user/login", sessionWrapper(app.userLogin, app))
 	mux.Handle("POST /user/login", sessionWrapper(app.userLoginPost, app))
-	mux.Handle("POST /user/logout", sessionWrapper(app.userLogoutPost, app))
+	mux.Handle("POST /user/logout", authSessionWrapper(app.userLogoutPost, app))
 
 	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
 }
 
 func sessionWrapper(route http.HandlerFunc, app *application) http.Handler {
 	return app.sessionManager.LoadAndSave(http.HandlerFunc(route))
+}
+
+func authSessionWrapper(route http.HandlerFunc, app *application) http.Handler {
+	return app.sessionManager.LoadAndSave(app.requireAuthentication(http.HandlerFunc(route)))
 }
