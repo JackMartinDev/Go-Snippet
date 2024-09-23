a6885a9c8077c63f291f2fcd1c276c1edd1443da
Jack Martin <jackmartin.dev@gmail.com>
2024-09-24 07:54:44 +0900

    Add tls encryption

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -47,6 +47,7 @@ func main() {
 	sessionManager := scs.New()
 	sessionManager.Store = postgresstore.New(db)
 	sessionManager.Lifetime = 12 * time.Hour
+	sessionManager.Cookie.Secure = true
 
 	app := &application{
 		logger:         logger,
@@ -64,7 +65,7 @@ func main() {
 
 	logger.Info("starting server", "addr", srv.Addr)
 
-	err = srv.ListenAndServe()
+	err = srv.ListenAndServeTLS("./tls/cert.pem", "./tls/key.pem")
 	logger.Error(err.Error())
 	os.Exit(1)
 }
